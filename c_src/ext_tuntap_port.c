6e5f462c1fa2d017312d341a3babce42cbe6ad03
James Ruan <ruanbeihong@gmail.com>
2015-05-26 19:24:48 +0800

    improve ext_tuntap_port.c

--- c_src/ext_tuntap_port.c
+++ c_src/ext_tuntap_port.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,105 +11,131 @@
 #include <sys/epoll.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
-
-
-char *executable = NULL;
-short mode = 0;
-short flags = IFF_NO_PI;
-
-
-struct option options[] = {
-	{"mode",         required_argument, NULL, 'm'},
-	{"multi_queue",  optional_argument, NULL, 'q'},
-	{"help",         no_argument,       NULL, 'h'},
-	{NULL,           no_argument,       NULL, 0}
-};
-
-
-void show_usage() {
-	fprintf(stderr, "Usage: %s [options] IFNAME\n", executable);
-	fprintf(stderr, "\n"
-		            "  -m, --mode=tun|tap\n"
-		            "  -q, --multi_queue          multiple queue\n"
-		            "  -h, --help                 print help message and exit\n");
-}
-
+#include <arpa/inet.h>
 
 int main (int argc, char **argv) {
-	executable = argv[0];
-
-	int opt, index;
-	while ((opt = getopt_long(argc, argv, "+m:qh", options, &index)) != -1) {
-		switch (opt) {
-		case 'm':
-			if (!strcmp(optarg, "tun")) {
-			  mode = IFF_TUN;
-			} else if (!strcmp(optarg, "tap")) {
-			  mode = IFF_TAP;
+	int open_device() {
+		void cmd_options (struct ifreq * ifr) {
+			void show_usage () {
+				fprintf(stderr, "Usage: %s [options] IFNAME\n", argv[0]);
+				fprintf(stderr, "\n"
+					            "  -m, --mode=tun|tap\n"
+					            "  -q, --multi_queue          multiple queue\n"
+					            "  -h, --help                 print help message and exit\n");
 			}
-			break;
-		case 'q':
-			flags |= IFF_MULTI_QUEUE;
-			break;
-		case 'h':
-		default:
-			show_usage();
+
+			struct option options[] = {
+				{"mode",         required_argument, NULL, 'm'},
+				{"multi_queue",  optional_argument, NULL, 'q'},
+				{"help",         no_argument,       NULL, 'h'},
+				{NULL,           no_argument,       NULL, 0}
+			};
+
+			if (optind >= argc) {
+				fprintf(stderr, "missing interface name\n");
+				show_usage();
+				exit(EXIT_FAILURE);
+			}
+
+			int opt, index;
+			while ((opt = getopt_long(argc, argv, "+m:qh", options, &index)) != -1) {
+				switch (opt) {
+				case 'm':
+					if (!strcmp(optarg, "tun")) {
+						ifr->ifr_flags |= IFF_TUN;
+					} else if (!strcmp(optarg, "tap")) {
+						ifr->ifr_flags |= IFF_TAP;
+					} else {
+						fprintf(stderr, "unknown mode\n");
+						show_usage();
+						exit(EXIT_FAILURE);
+					}
+					break;
+				case 'q':
+					ifr->ifr_flags |= IFF_MULTI_QUEUE;
+					break;
+				case 'h':
+				default:
+					show_usage();
+					exit(EXIT_FAILURE);
+				}
+			}
+
+			if(!((ifr->ifr_flags & IFF_TUN) | (ifr->ifr_flags & IFF_TAP))) {
+				fprintf(stderr, "missing mode\n");
+				show_usage();
+				exit(EXIT_FAILURE);
+			}
+
+			ifr->ifr_flags |= IFF_NO_PI;
+			strncpy(ifr->ifr_name, argv[optind], IFNAMSIZ);
+		}
+
+		struct ifreq ifr;
+		memset(&ifr, 0, sizeof(ifr));
+
+		int fd = open("/dev/net/tun", O_RDWR);
+		if (fd < 0) {
+			perror("ERROR: failed to open tun device");
+			exit(EXIT_FAILURE);
+		}
+
+		cmd_options(&ifr);
+
+		if (ioctl(fd, TUNSETIFF, &ifr) < 0) {
+			perror("ERROR: ioctl");
+			exit(EXIT_FAILURE);
+		}
+		return(fd);
+	}
+
+	void epoll_add(uint32_t events, int poll, int fd) {
+		struct epoll_event ev = {0};
+		ev.events = events;
+		ev.data.fd = fd;
+		if (epoll_ctl(poll, EPOLL_CTL_ADD, fd, &ev) < -1) {
+			perror("epoll_ctl: ADD");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	if (optind >= argc) {
-		fprintf(stderr, "missing interface name\n");
-		show_usage();
-		exit(EXIT_FAILURE);
-	}
-	
-	if (!mode) {
-		fprintf(stderr, "missing mode\n");
-		show_usage();
-		exit(EXIT_FAILURE);
-	}
-	
-	int fd = open("/dev/net/tun", O_RDWR);
-	if (fd < 0) {
-		perror("ERROR: failed to open tun device");
-		exit(EXIT_FAILURE);
+	void epoll_mod(uint32_t events, int poll, int fd) {
+		struct epoll_event ev = {0};
+		ev.events = events;
+		ev.data.fd = fd;
+		if (epoll_ctl(poll, EPOLL_CTL_MOD, fd, &ev) < -1) {
+			perror("epoll_ctl: MOD");
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	struct ifreq ifr;
-	ifr.ifr_flags = flags | mode;
-	strncpy(ifr.ifr_name, argv[optind], IFNAMSIZ);
-	
-	if (ioctl(fd, TUNSETIFF, &ifr) < 0) {
-		perror("ERROR: ioctl");
-		exit(EXIT_FAILURE);
-	}
-	
+	int const packet_len_byte_No = 2;
+	int const BUFSIZE = 65536 + packet_len_byte_No;
+	ssize_t from_port_length;
+	char from_port_buffer[BUFSIZE];
+	ssize_t to_port_length;
+	char to_port_buffer[BUFSIZE];
+	struct epoll_event event;
+	uint32_t tun_event = 0;
+
+	int device_fd = open_device();
 	int poll = epoll_create(1);
+	/* one event once */
 
-	struct epoll_event event = {0};
-	uint32_t tun_events = EPOLLIN;
-
-	event.events = EPOLLIN;
-	event.data.fd = STDIN_FILENO;
-	epoll_ctl(poll, EPOLL_CTL_ADD, STDIN_FILENO, &event);
-	
-	event.events = 0;
-	event.data.fd = STDOUT_FILENO;
-	epoll_ctl(poll, EPOLL_CTL_ADD, STDOUT_FILENO, &event);
-	
-	event.events = tun_events;
-	event.data.fd = fd;
-	epoll_ctl(poll, EPOLL_CTL_ADD, fd, &event);
-
-	ssize_t from_port_length = 0;
-	char from_port_buffer[2048];
-	ssize_t to_port_length = 0;
-	char to_port_buffer[2048];
+	/*
+     * PORT_OUT(STDIN)  -> from_port_buffer -> DEV
+	 * IN
+	 * PORT_IN(STDOUT)  <- to_port_buffer   <- DEV
+	 *                                         IN
+	 */
+	epoll_add(EPOLLIN, poll, STDIN_FILENO);
+	epoll_add(0, poll, STDOUT_FILENO);
+	epoll_add(EPOLLIN, poll, device_fd);
 
 	for (;;) {
 		int nfds = epoll_wait(poll, &event, 1, -1);
-		
+		/* one event once */
+
 		if (nfds == -1) {
 			if (errno == EINTR)
 				continue;
@@ -117,52 +144,90 @@ int main (int argc, char **argv) {
 			exit(EXIT_FAILURE);
 		}
 
-		int event_fd = event.data.fd;
-		if (event_fd == fd) {
-    		if (event.events == EPOLLIN) {
-    			to_port_length = read(fd, to_port_buffer+2, 2046);
-    			*(unsigned char *)to_port_buffer = (to_port_length >> 8) & 0xFF;       
-    			*(unsigned char *)(to_port_buffer+1) = to_port_length & 0xFF;
+		if (event.data.fd == STDIN_FILENO) {
+			/* port is ready to read */
+			/*
+			 * PORT_OUT(STDIN)  -> from_port_buffer -> DEV
+			 * *IN*
+			 * ===>
+			 * PORT_OUT(STDIN)  -> from_port_buffer -> DEV
+			 *                     DATA                OUT
+			 */
+			epoll_mod(0, poll, STDIN_FILENO);
+			tun_event |= EPOLLOUT;
+			epoll_mod(tun_event, poll, device_fd);
 
-    			event.events = EPOLLOUT;
-    			event.data.fd = STDOUT_FILENO;
-    			epoll_ctl(poll, EPOLL_CTL_MOD, STDOUT_FILENO, &event);
+			if (read(STDIN_FILENO, from_port_buffer, packet_len_byte_No) == -1) {
+				perror("read STDIN");
+				exit(EXIT_FAILURE);
+			}
+			assert(packet_len_byte_No == 2);
+			from_port_length = ntohs(*((uint16_t*)from_port_buffer));
+			if (read(STDIN_FILENO, from_port_buffer + packet_len_byte_No, from_port_length) == -1) {
+				perror("read STDIN");
+				exit(EXIT_FAILURE);
+			}
+		} else
+		if (event.data.fd == STDOUT_FILENO) {
+			/* port is ready to write */
+			/*
+			 * PORT_IN(STDOUT)  <- to_port_buffer   <- DEV
+			 * *OUT*               DATA
+			 * ===>
+			 * PORT_IN(STDOUT)  <- to_port_buffer   <- DEV
+			 * DATA                                    IN
+			 */
+			epoll_mod(0, poll, STDOUT_FILENO);
+			tun_event |= EPOLLIN;
+			epoll_mod(tun_event, poll, device_fd);
 
-    			tun_events &= ~EPOLLIN;
-    		} else if (event.events == EPOLLOUT) {
-    			write(fd, from_port_buffer+2, from_port_length);
+			if (write(STDOUT_FILENO, to_port_buffer, to_port_length + packet_len_byte_No) == -1) {
+				perror("write STDOUT");
+				exit(EXIT_FAILURE);
+			}
+		} else
+		if (event.data.fd == device_fd) {
+			if (event.events == EPOLLIN) {
+				/* device is ready to read */
+				/*
+				 * PORT_IN(STDOUT)  <- to_port_buffer   <- DEV
+				 *                                         *IN*
+				 * ==>
+				 * PORT_IN(STDOUT)  <- to_port_buffer   <- DEV
+				 * OUT                 DATA
+				*/
+				epoll_mod(EPOLLOUT, poll, STDOUT_FILENO);
+				tun_event &= ~EPOLLIN;
+				epoll_mod(tun_event, poll, device_fd);
 
-    			event.events = EPOLLIN;
-    			event.data.fd = STDIN_FILENO;
-    			epoll_ctl(poll, EPOLL_CTL_MOD, STDIN_FILENO, &event);
+				to_port_length = read(device_fd, to_port_buffer + packet_len_byte_No, BUFSIZE - packet_len_byte_No);
+				if (to_port_length == -1) {
+					perror("read tun device");
+					exit(EXIT_FAILURE);
+				}
+				uint16_t length = htons(to_port_length);
+				memcpy(to_port_buffer, &length, packet_len_byte_No);
+			} else
+			if (event.events == EPOLLOUT) {
+				/* device is ready to write */
+                /*
+				 * PORT_OUT(STDIN)  -> from_port_buffer -> DEV
+				 *                     DATA                *OUT*
+				 * ==>
+				 * PORT_OUT(STDIN)  -> from_port_buffer -> DEV
+				 * IN                                      DATA
+				 */
+				epoll_mod(EPOLLIN, poll, STDIN_FILENO);
+				tun_event &= ~EPOLLOUT;
+				epoll_mod(tun_event, poll, device_fd);
 
-    			tun_events &= ~EPOLLOUT;
-    		}
-		} else if (event_fd == STDIN_FILENO) {
-			if (read(STDIN_FILENO, from_port_buffer, 2) <= 0) break;
-		
-			from_port_length = (((unsigned char)from_port_buffer[0]) << 8) | ((unsigned char)from_port_buffer[1]);
-			read(STDIN_FILENO, from_port_buffer+2, from_port_length);
-		
-			event.events = 0;
-			event.data.fd = event_fd;
-			epoll_ctl(poll, EPOLL_CTL_MOD, event_fd, &event);
-		
-			tun_events |= EPOLLOUT;
-		} else if (event_fd == STDOUT_FILENO) {
-	  	  write(STDOUT_FILENO, to_port_buffer, to_port_length+2);
-		
-	  	  event.events = 0;
-	  	  event.data.fd = event_fd;
-	  	  epoll_ctl(poll, EPOLL_CTL_MOD, event_fd, &event);
-		
-	  	  tun_events |= EPOLLIN;
+				if (write(device_fd, from_port_buffer + packet_len_byte_No, from_port_length) == -1) {
+					perror("write tun device");
+					exit(EXIT_FAILURE);
+				}
+			}
 		}
-		
-		event.events = tun_events;
-		event.data.fd = fd;
-		epoll_ctl(poll, EPOLL_CTL_MOD, fd, &event);
 	}
 
-  return 0;
+  return(EXIT_SUCCESS);
 }
