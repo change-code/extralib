6e7dfe829ef2cdda30d15efe2d33b1fcf61e2470
James Ruan <ruanbeihong@gmail.com>
2015-05-26 23:07:22 +0800

    reformat spaces to tabs

--- src/ext_tuntap_port.erl
+++ src/ext_tuntap_port.erl
@@ -4,36 +4,36 @@
 
 
 start(Options) ->
-    Args = [<<"tap0">>],
+	Args = [<<"tap0">>],
 
-    Mode =
-        case proplists:get_value(mode, Options, tap) of
-            tun ->
-                <<"tun">>;
-            tap ->
-                <<"tap">>
-        end,
+	Mode =
+		case proplists:get_value(mode, Options, tap) of
+			tun ->
+				<<"tun">>;
+			tap ->
+				<<"tap">>
+		end,
 
-    Args1 = [<<"--mode=", Mode/binary>>|Args],
-    Args2 =
-        case proplists:is_defined(multi_queue, Options) of
-            true ->
-                [<<"--multi_queue">>|Args1];
-            false ->
-                Args1
-        end,
+	Args1 = [<<"--mode=", Mode/binary>>|Args],
+	Args2 =
+		case proplists:is_defined(multi_queue, Options) of
+			true ->
+				[<<"--multi_queue">>|Args1];
+			false ->
+				Args1
+		end,
 
-    Dir = code:lib_dir(extralib, priv),
+	Dir = code:lib_dir(extralib, priv),
 
-    Pid =
-        erlang:open_port(
-          {spawn_executable, [Dir, "/ext_tuntap_port"]},
-          [{packet, 2},
-           binary,
-           {args, Args2}]),
+	Pid =
+		erlang:open_port(
+		  {spawn_executable, [Dir, "/ext_tuntap_port"]},
+		  [{packet, 2},
+		   binary,
+		   {args, Args2}]),
 
-    {ok, Pid}.
+	{ok, Pid}.
 
 
 send(Port, Data) ->
-    erlang:port_command(Port, Data).
+	erlang:port_command(Port, Data).
